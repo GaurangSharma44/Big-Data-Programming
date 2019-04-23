85c48eb34b4e0ddeb34759eb410e3deee6d07a45
Gaurang Sharma <43086300+slyrangoon@users.noreply.github.com>
2019-04-24 00:01:21 +0200

    Create LongestCommonSequence.py

---
+++ LongestCommonSequence.py
@@ -0,0 +1,33 @@
+def LongestCommonSubsequece():
+    
+    take_input = True
+    while(take_input):
+        S1 = input("Enter your first string: ")
+        S2 = input("Enter your second string: ")
+
+        if not S1.isalpha() or not S2.isalpha():
+            print("Please use only alpha characters")
+            take_input = True
+        else:
+            take_input = False
+
+    s1_array = list(S1)
+    s2_array = list(S2)
+
+    temp = []
+    index = 0
+    for i in range(len(s1_array)):
+        for j in range(len(s2_array))[index:]:
+            if s1_array[i].lower() == s2_array[j].lower():
+                temp.append(s2_array[j])
+                index = j+1
+            else:
+                continue
+            break
+    S3 = temp
+    print ("The Longest Common Subsequence is")
+    for i in S3:
+        print(i, end="")
+
+if __name__ == "__main__":
+    LongestCommonSubsequece()
